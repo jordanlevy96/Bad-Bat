d4d60d837ac411c034b2e790779a4d2488ed6719
jordanlevy96 <jordanlevy96@gmail.com>
2024-06-16 23:45:45 -0700

    move title to own scene; fix instruction display

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,34 +1,13 @@
 extends CanvasLayer
 
-# Notifies `Main` node that the button has been pressed
-signal start_game
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if Input.is_action_pressed("start"):
-		_on_start_button_pressed()
-
-
-func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
-	
-
-	$Message.text = "Hide from the Enemies!"
-	$Message.show()
-	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.0).timeout
-	$StartButton.show()
+func _ready():
+	$Instructions.hide()
 
 func _on_start_button_pressed():
 	$StartButton.hide()
 	$Logo.hide()
-	start_game.emit()
+	$Instructions.show()
+	$MessageTimer.start()
 
 func _on_message_timer_timeout():
-	$ColorRect.hide()
-	$Message.hide()
-	queue_free()
-
-
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
