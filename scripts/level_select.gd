d42e940d1b592d1ae5be1d14947f022084f1d8e7
Jordan Levy <jordan.levy@bigspark.dev>
2024-06-27 12:03:13 -0700

    add level to select screen

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -8,5 +8,8 @@ func _on_tutorial_button_pressed():
 	print($MessageTimer)
 
 func _on_message_timer_timeout():
-	print('timeout')
 	get_tree().change_scene_to_file("res://scenes/levels/tutorial.tscn")
+
+
+func _on_level_01_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
